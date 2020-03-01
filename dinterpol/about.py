e7c1d72119a27b540c7ec6bd9e93ddc16243a1a8
João Pinto <lamego.pinto@gmail.com>
2020-03-01 22:28:21 +0100

    Bump to release 0.3.3

--- dinterpol/about.py
+++ dinterpol/about.py
@@ -1,5 +1,5 @@
 __title__ = "dinterpol"
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __summary__ = "data interpolation templating engine"
 __uri__ = "https://github.com/Openpipe/dinterpol"
 __author__ = "João Pinto"
